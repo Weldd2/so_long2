8af06f543398096dd8e24c31b7032aab5a07a33f
Your Name <you@example.com>
2024-06-01 20:43:29 +0200

    trim

--- srcs/utils/utils.c
+++ srcs/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: antoinemura <antoinemura@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 16:04:13 by antoinemura       #+#    #+#             */
-/*   Updated: 2024/05/26 16:22:19 by antoinemura      ###   ########.fr       */
+/*   Updated: 2024/06/01 20:43:23 by antoinemura      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,11 @@ char	*gnl_and_trim(int fd)
 	if (line != NULL)
 	{
 		line = ft_strtrim(line, "\n");
+		if (!line)
+		{
+			free(tmp);
+			return NULL;
+		}
 		free(tmp);
 	}
 	return (line);
